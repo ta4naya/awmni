3f089227a7c353fb901601cb3e1de9ea38fe0bb3
ta4naya <ta4naya@yahoo.com>
2019-03-22 18:07:11 +0100

    Schema and model Update

---
+++ server/models/music.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+ // Initialize the Schema
+ const musicSchema = new Schema({
+     title : {type: String, required: true, max:[128, 'Too Long Max is 128']},
+     description:{type: String, required:true},
+     audioUrl:{type: String,required:true},
+     speaker:{type:String,required:true},
+     
+
+ });
+
